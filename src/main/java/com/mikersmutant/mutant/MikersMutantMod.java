9b5c66a59fe4cec52425130af33b77e34ad893a7
Mikers01 <seltor.11@mail.ru>
2026-04-15 15:46:50 +0800

    Update MikersMutantMod.java

--- src/main/java/com/mikersmutant/mutant/MikersMutantMod.java
+++ src/main/java/com/mikersmutant/mutant/MikersMutantMod.java
@@ -6,13 +6,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraft.client.renderer.entity.EntityRenderers;
-import com.mikersmutant.mutant.entity.MutantRenderer;
 
 @Mod(MikersMutantMod.MOD_ID)
 public class MikersMutantMod {
@@ -25,14 +18,5 @@ public class MikersMutantMod {
         ModSounds.register(modEventBus);
         ModBlocks.register(modEventBus);
         MinecraftForge.EVENT_BUS.register(this);
-    }
-}
-@Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public static class ClientModEvents {
-    @SubscribeEvent
-    public static void onClientSetup(FMLClientSetupEvent event) {
-        event.enqueueWork(() -> {
-            //EntityRenderers.register(ModEntities.MUTANT.get(), MutantRenderer::new);
-        });
     }
 }
