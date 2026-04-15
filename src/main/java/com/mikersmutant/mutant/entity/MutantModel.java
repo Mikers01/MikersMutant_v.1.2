f04d19840c4cac712381ebd46f043be04134a671
Mikers01 <seltor.11@mail.ru>
2026-04-15 15:07:18 +0800

    Update MutantModel.java

--- src/main/java/com/mikersmutant/mutant/entity/MutantModel.java
+++ src/main/java/com/mikersmutant/mutant/entity/MutantModel.java
@@ -2,17 +2,20 @@ package com.mikersmutant.mutant.entity;
 
 import com.mikersmutant.mutant.MikersMutantMod;
 import net.minecraft.resources.ResourceLocation;
-import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib.model.GeoModel;
 
-public class MutantModel extends AnimatedGeoModel<MutantEntity> {
+public class MutantModel extends GeoModel<MutantEntity> {
+    @Override
     public ResourceLocation getModelResource(MutantEntity object) {
         return new ResourceLocation(MikersMutantMod.MOD_ID, "geo/mutant.geo.json");
     }
 
+    @Override
     public ResourceLocation getTextureResource(MutantEntity object) {
         return new ResourceLocation(MikersMutantMod.MOD_ID, "textures/entity/mutant.png");
     }
 
+    @Override
     public ResourceLocation getAnimationResource(MutantEntity animatable) {
         return new ResourceLocation(MikersMutantMod.MOD_ID, "animations/mutant.animation.json");
     }
